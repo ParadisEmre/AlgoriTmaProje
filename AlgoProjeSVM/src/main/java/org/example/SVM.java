4fe4237c1a478e36e96c2914e86b007d2060f189
amithatdemirkol <72972673+amithatdemirkol@users.noreply.github.com>
2023-01-05 19:38:27 +0300

    svm init

--- AlgoProjeSVM/src/main/java/org/example/SVM.java
+++ AlgoProjeSVM/src/main/java/org/example/SVM.java
@@ -3,14 +3,18 @@ package org.example;
 import java.util.List;
 
 public class SVM {
-
+    public SVM(int iterationCount) {
+       this.iterationCount=iterationCount;
+    }
+    public double bias;
+    public Vector3 weight;
+    public int iterationCount;
     public void train(List<DataEntry> dataSet){
         //TODO
     }
-    public float predict(Vector3 data){
-                
-        //return Math.signum(Vector3.dot(xi,weight)-bias);
-        
-        return 1.0f;
+    // abs (xi. weight)-bias 
+    public float predict(Vector3 xi){
+            
+            return (float) Math.signum(Vector3.dot(xi,weight)-bias);//hey
     }
 }
